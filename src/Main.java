dd0d21df51ac79e99c735e8151b838104c0c4bf8
felype-carvalho <43687521+felype-carvalho@users.noreply.github.com>
2021-03-09 22:15:27 -0300

    Adição Termo de Responsabilidade

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+/*
+Entrega de trabalho
+Nós,
+Fabricio Viana Belomo
+Felype Carvalho de Oliveira
+declaramos que
+todas as respostas são fruto de nosso próprio trabalho,
+não copiamos respostas de colegas externos à equipe,
+não disponibilizamos nossas respostas para colegas externos à equipe e
+não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar outros.
+*/
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
