5b4f81ffcc71185bd293bbddeae58346a8de1b6b
Fabricio Belomo <fabriciobelomo@gmail.com>
2021-03-09 23:38:35 -0300

    Iniciada a leitura do arquivo texto

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 /*
 Entrega de trabalho
 Nós,
@@ -9,11 +10,29 @@ não copiamos respostas de colegas externos à equipe,
 não disponibilizamos nossas respostas para colegas externos à equipe e
 não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar outros.
 */
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static int[][] lerEntradaTxt(String nomeArquivo) throws Exception {
+        FileReader arquivo = new FileReader(nomeArquivo);
+        BufferedReader leitorArq = new BufferedReader(arquivo);
+        String linhaArquivo = "";
+
+        do {
+            linhaArquivo = leitorArq.readLine();
+            if (linhaArquivo != null) {
+                System.out.println("Linhas: " + linhaArquivo);
+            }
+        }
+        while (linhaArquivo != null);
+
+        leitorArq.close();
+        return null;
     }
 
-    
+    public static void main(String[] args) throws Exception {
+        int[][] matriz = lerEntradaTxt("entrada.txt");
+    }
 }
